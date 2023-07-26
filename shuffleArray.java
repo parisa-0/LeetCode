b3fa73f48cc4c8321af0610e123756ee1b032e09
Parisa <134794618+parisa-0@users.noreply.github.com>
2023-07-26 14:26:57 +0100

    Update shuffleArray.java

--- shuffleArray.java
+++ shuffleArray.java
@@ -7,15 +7,10 @@ package SolvedLeetCode;
             public int[] shuffle(int[] nums, int n) {
                 int[] newNums = new int [nums.length];
 
-                for(int i = 0; i < nums.length/2; i++){
-                    newNums[i*2] = nums[i];
+                for (int i = 0; i < n; i++) {
+                newNums[i * 2] = nums[i];
+                newNums[(i * 2) + 1] = nums[i + n];
                 }
-
-                for(int j = nums.length/2; j < nums.length; j++){
-                    newNums[(j-nums.length/2) * 2 + 1] = nums[j];
-                }
-
-
                 return newNums;
             }
         }
