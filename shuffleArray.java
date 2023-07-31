9891b18b86eb027bb3aa109519ea8563a7e6ade4
Parisa <134794618+parisa-0@users.noreply.github.com>
2023-07-31 16:26:04 +0100

    Update shuffleArray.java

--- shuffleArray.java
+++ shuffleArray.java
@@ -3,7 +3,7 @@ package SolvedLeetCode;
         //https://leetcode.com/problems/shuffle-the-array/
         //Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
         //Return the array in the form [x1,y1,x2,y2,...,xn,yn].
-        class shuffleArray {
+        class ShuffleArray {
             public int[] shuffle(int[] nums, int n) {
                 int[] newNums = new int [nums.length];
 
