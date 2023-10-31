f7913055044cddbb339f73cb287e597073f4af98
Parisa <134794618+parisa-0@users.noreply.github.com>
2023-10-31 21:06:58 +0000

    Create uniqueOccurrences.java

---
+++ uniqueOccurrences.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        
+  
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        Set<Integer> myHashSet = new HashSet<>(map.values());
+
+        return map.size() == myHashSet.size();
+    }
+}
