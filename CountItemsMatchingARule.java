fd837b699fe4ab5c9d25afff139e74532331c6a5
Parisa <134794618+parisa-0@users.noreply.github.com>
2024-01-10 11:10:48 +0000

    Create CountItemsMatchingARule.java

---
+++ CountItemsMatchingARule.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+
+            int count = 0;
+
+            for(int i = 0; i < items.size(); i++) {
+                for(int j = 0; j < items.get(i).size(); j++) {
+                    if(ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)) {
+                        count++;
+                        break;
+                    }
+                    else if(ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)) {
+                        count++;
+                        break;
+                    }
+                    else if(ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue)) {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            return count;
+    }
+}
