71dd8de5c4a4a2bd574a6318768e58615d59531a
Parisa <134794618+parisa-0@users.noreply.github.com>
2024-01-27 18:40:58 +0000

    Update ContainsDuplicate.java

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -4,6 +4,19 @@ import java.util.Arrays;
 
 public class ContainsDuplicate {
 
+    class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> mySet = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(!mySet.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
 //https://leetcode.com/problems/contains-duplicate/
     //Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
